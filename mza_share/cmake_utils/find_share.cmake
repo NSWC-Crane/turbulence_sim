bc44dcd386fe55b31956e211f4451261994dfaa0
davemers0160 <davemers@iu.edu>
2022-01-26 15:51:56 -0500

    Update find_share.cmake

--- mza_share/cmake_utils/find_share.cmake
+++ mza_share/cmake_utils/find_share.cmake
@@ -2,14 +2,14 @@ message(STATUS "----------------------------------------------------------------
 message(STATUS "Looking for the MZA SHaRE Library...")
 
 find_path(SHaRE_INCLUDE_DIRS MzaShare.h
-    PATHS /usr/local /opt/MZA /home/$ENV{HOME}/MZA "C:/MZA/ScalingCode/2021a/ScalingCodeAPI" "D:/MZA/ScalingCode/2021a/ScalingCodeAPI" "E:/MZA/ScalingCode/2021a/ScalingCodeAPI" ENV CPATH
+    PATHS /usr/local /opt/MZA /home/$ENV{HOME}/MZA $ENV{SCALINGCODEAPI_PATH} "C:/MZA/ScalingCode/2021a/ScalingCodeAPI" "D:/MZA/ScalingCode/2021a/ScalingCodeAPI"  ENV CPATH
     PATH_SUFFIXES include
     )
 
 find_library(SHaRE_LIBS MzaShare
     HINTS ${FTDI_INCLUDE_DIRS}
-    PATHS /usr/local /opt/MZA /home/$ENV{HOME}/MZA "C:/MZA/ScalingCode/2021a/ScalingCodeAPI" "D:/MZA/ScalingCode/2021a/ScalingCodeAPI" "E:/MZA/ScalingCode/2021a/ScalingCodeAPI"
-    PATH_SUFFIXES lib lib64 lib/windows/msvc15/x64
+    PATHS /usr/local /opt/MZA /home/$ENV{HOME}/MZA $ENV{SCALINGCODEAPI_PATH} "C:/MZA/ScalingCode/2021a/ScalingCodeAPI" "D:/MZA/ScalingCode/2021a/ScalingCodeAPI" 
+    PATH_SUFFIXES lib lib64 lib/windows/msvc15/x64 lib/linux/x64 lib/${PLATFORM_OS}/${PLATFORM_TARGET} 
     )
     
 mark_as_advanced(SHaRE_LIBS SHaRE_INCLUDE_DIRS)
