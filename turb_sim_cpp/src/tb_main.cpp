ce02857ebff9324c04ecedb9f242288692e704cf
Dave <davemers@iu.edu>
2022-09-02 19:46:13 -0400

    adding new bessel functions

--- turb_sim_cpp/src/tb_main.cpp
+++ turb_sim_cpp/src/tb_main.cpp
@@ -39,7 +39,7 @@ typedef void* HINSTANCE;
 
 #include "cv_dft_conv.h"
 #include "motion_compensate.h"
-
+#include "turbulence_sim.h"
 
 // ----------------------------------------------------------------------------------------
 //bool compare(std::pair<uint8_t, uint8_t> p1, std::pair<uint8_t, uint8_t> p2)
@@ -101,6 +101,100 @@ static double bessj0(double x)
     }
     return ans;
 }
+
+static double bessj1( double x )
+/*------------------------------------------------------------*/
+/* PURPOSE: Evaluate Bessel function of first kind and order  */
+/*          1 at input x                                      */
+/*------------------------------------------------------------*/
+{
+   double ax,z;
+   double xx,y,ans,ans1,ans2;
+
+   if ((ax=fabs(x)) < 8.0) {
+      y=x*x;
+      ans1=x*(72362614232.0+y*(-7895059235.0+y*(242396853.1
+         +y*(-2972611.439+y*(15704.48260+y*(-30.16036606))))));
+      ans2=144725228442.0+y*(2300535178.0+y*(18583304.74
+         +y*(99447.43394+y*(376.9991397+y*1.0))));
+      ans=ans1/ans2;
+   } else {
+      z=8.0/ax;
+      y=z*z;
+      xx=ax-2.356194491;
+      ans1=1.0+y*(0.183105e-2+y*(-0.3516396496e-4
+         +y*(0.2457520174e-5+y*(-0.240337019e-6))));
+      ans2=0.04687499995+y*(-0.2002690873e-3
+         +y*(0.8449199096e-5+y*(-0.88228987e-6
+         +y*0.105787412e-6)));
+      ans=sqrt(0.636619772/ax)*(cos(xx)*ans1-z*sin(xx)*ans2);
+      if (x < 0.0) ans = -ans;
+   }
+   return ans;
+}
+
+double bessj( int n, double x )
+/*------------------------------------------------------------*/
+/* PURPOSE: Evaluate Bessel function of first kind and order  */
+/*          n at input x                                      */
+/* The function can also be called for n = 0 and n = 1.       */
+/*------------------------------------------------------------*/
+{
+   int    j, jsum, m;
+   double ax, bj, bjm, bjp, sum, tox, ans;
+
+
+   if (n < 0)
+   {
+      double   dblank;
+      setdblank_c( &dblank );
+      return( dblank );
+   }
+   ax=fabs(x);
+   if (n == 0)
+      return( bessj0(ax) );
+   if (n == 1)
+      return( bessj1(ax) );
+      
+
+   if (ax == 0.0)
+      return 0.0;
+   else if (ax > (double) n) {
+      tox=2.0/ax;
+      bjm=bessj0(ax);
+      bj=bessj1(ax);
+      for (j=1;j<n;j++) {
+         bjp=j*tox*bj-bjm;
+         bjm=bj;
+         bj=bjp;
+      }
+      ans=bj;
+   } else {
+      tox=2.0/ax;
+      m=2*((n+(int) sqrt(ACC*n))/2);
+      jsum=0;
+      bjp=ans=sum=0.0;
+      bj=1.0;
+      for (j=m;j>0;j--) {
+         bjm=j*tox*bj-bjp;
+         bjp=bj;
+         bj=bjm;
+         if (fabs(bj) > BIGNO) {
+            bj *= BIGNI;
+            bjp *= BIGNI;
+            ans *= BIGNI;
+            sum *= BIGNI;
+         }
+         if (jsum) sum += bj;
+         jsum=!jsum;
+         if (j == n) ans=bjp;
+      }
+      sum=2.0*sum-bj;
+      ans /= sum;
+   }
+   return  x < 0.0 && n%2 == 1 ? -ans : ans;
+}
+
 
 
 double BESSJ0(double X) {
