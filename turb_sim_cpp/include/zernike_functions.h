0b0cfe129dbdecb47ee1adc47021982284d761b3
Dave <davemers@iu.edu>
2022-09-11 21:58:17 -0400

    more zernike functions

--- turb_sim_cpp/include/zernike_functions.h
+++ turb_sim_cpp/include/zernike_functions.h
@@ -9,21 +9,24 @@
 #include <opencv2/highgui.hpp>
 #include <opencv2/imgproc.hpp>
 
-//-----------------------------------------------------------------------------
-void noll_2_zern_index(int32_t j, int32_t &n, int32_t &m)
-{
-    /*
-    This function maps the input "j" to the (row, column) of the Zernike pyramid using the Noll numbering scheme.
+#include "turbulence_param.h"
+#include "turbulence_sim.h"
+#include "opencv_helper.h"
 
-    Authors: Tim van Werkhoven, Jason Saredy
-    See: https://github.com/tvwerkhoven/libtim-py/blob/master/libtim/zern.py
-    */
-    
+//-----------------------------------------------------------------------------
+//This function maps the input "j" to the (row, column) of the Zernike pyramid using the Noll numbering scheme.
+//
+//Authors: Tim van Werkhoven, Jason Saredy
+//See: https://github.com/tvwerkhoven/libtim-py/blob/master/libtim/zern.py
+//
+void noll2zernike_index(int64_t j, int64_t &n, int64_t &m)
+{
+
     if (j == 0)
         std::cout << "Noll indices start at 1, 0 is invalid." << std::endl;
         
     n = 0;
-    int32_t j1 = j - 1;
+    int64_t j1 = j - 1;
     
     while (j1 > n)
     {
@@ -36,89 +39,312 @@ void noll_2_zern_index(int32_t j, int32_t &n, int32_t &m)
 
 }   // end of noll_2_zern_index
 
-//-----------------------------------------------------------------------------
-cv::Mat noll_cov_matrix(Z, D, fried):
-{
-    /*
-    This function generates the covariance matrix for a single point source. See the associated paper for details on
-    the matrix itself.
 
-    :param Z: Number of Zernike basis functions/coefficients, determines the size of the matrix.
-    :param D: The diameter of the aperture (meters)
-    :param fried: The Fried parameter value
-    :return:
-    */
-    
-    int32_t idx, jdx;
-    float coef1, num, den;
-    cv::Mat cov = cv::Mat::zeros(Z, Z, CV_64FC1);
-    //C = np.zeros((Z,Z))
-    
-    
+//-----------------------------------------------------------------------------
+//This function generates the covariance matrix for a single point source. See the associated paper for details on
+//the matrix itself.
+
+//:param Z: Number of Zernike basis functions/coefficients, determines the size of the matrix.
+//:param D: The diameter of the aperture (meters)
+//:param fried: The Fried parameter value
+//
+cv::Mat noll_covariance_matrix(uint32_t Z, double D, double r0)
+{   
+    uint32_t idx, jdx;
+    double c1, num, den;
+
+    int64_t ni, mi, nj, mj;
+
+    cv::Mat dst = cv::Mat::zeros(Z, Z, CV_64FC1);
+
     for (idx=0; idx<Z; ++idx)
     {
         for (jdx=0; jdx<Z; ++jdx)
         {
             //ni, mi = nollToZernInd(idx+1)
             //nj, mj = nollToZernInd(jdx+1)
-            
-            noll_2_zern_index(idx+1, ni, mi);
-            noll_2_zern_index(jdx+1, nj, mj);
+            noll2zernike_index(idx + 1, ni, mi);
+            noll2zernike_index(jdx + 1, nj, mj);
                        
-            if (abs(mi) == abs(mj)) and (np.mod(idx - jdx, 2) == 0):
+            // if (abs(mi) == abs(mj)) and (np.mod(i - j, 2) == 0):
+            if ((abs(mi) == abs(mj)) & ((idx - jdx) % 2 == 0))
             {
-                num = tgamma(14.0/3.0) * tgamma(((float)(ni + nj) - 5.0/3.0)/2.0);
+                // num = math.gamma(14.0/3.0) * math.gamma((ni + nj - 5.0/3.0)/2.0)
+                num = tgamma(14.0/3.0) * tgamma(((double)(ni + nj) - 5.0/3.0)/2.0);
                 
-                den = tgamma(((float)(-ni + nj) + 17.0/3.0)/2.0) * tgamma(((float)(ni - nj) + 17.0/3.0)/2.0) * tgamma(((float)(ni + nj) + 23.0/3.0)/2.0);
+                // den = math.gamma((-ni + nj + 17.0/3.0)/2.0) * math.gamma((ni - nj + 17.0/3.0)/2.0) * math.gamma((ni + nj + 23.0/3.0)/2.0)
+                den = tgamma(((double)(-ni + nj) + 17.0/3.0)/2.0) * tgamma(((double)(ni - nj) + 17.0/3.0)/2.0) * tgamma(((double)(ni + nj) + 23.0/3.0)/2.0);
                       
-                coef1 = 0.0072 * powf(cv::pi,(8.0/3.0)) * powf((D/fried), (5.0/3.0)) * sqrt((ni + 1) * (nj + 1)) * ((((ni + nj - 2*abs(mi)) >> 1)&0x01 == 1) ? -1.0 : 1.0);
-                        
+                // coef1 = 0.0072 * (np.pi ** (8.0/3.0)) * ((D/fried) ** (5.0/3.0)) * np.sqrt((ni + 1) * (nj + 1)) * ((-1) ** ((ni + nj - 2*abs(mi))/2.0))
+                // x^(y) = std::exp(y * std::log(x))                
+                c1 = 0.0072 * std::exp((8.0 / 3.0) * std::log(CV_PI)) * std::exp((5.0 / 3.0) * std::log(D / r0));
+                c1 *= std::sqrt((ni + 1) * (nj + 1)) * ((((ni + nj - 2 * abs(mi)) >> 1) & 0x01 == 1) ? -1.0 : 1.0);
+
                 //C[i, j] = coef1*num/den
-                cov.at<float>(idx, jdx) = coef1*num/den;
+                dst.at<double>(idx, jdx) = c1*num/den;
             }
             else
             {
-                cov.at<float>(idx, jdx) = 0.0;
+                dst.at<double>(idx, jdx) = 0.0;
             }
         }
     }
     
-    cov.at<float>(0,0) = 1.0;
-    return cov;
+    dst.at<double>(0,0) = 1.0;
+
+    return dst;
 }   // end of noll_cov_matrix
- 
+
 
 //-----------------------------------------------------------------------------
+//Just a simple function to generate random coefficients as needed, conforms to Zernike's Theory. The nollCovMat()
+//function is at the heart of this function.
 
-cv::Mat gen_zernike_coeff(num_zern, d_r0):
+//A note about the function call of nollCovMat in this function. The input (..., 1, 1) is done for the sake of
+//flexibility. One can call the function in the typical way as is stated in its description. However, for
+//generality, the D/r0 weighting is pushed to the "b" random vector, as the covariance matrix is merely scaled by
+//such value.
+
+//:param num_zern: This is the number of Zernike basis functions/coefficients used. Should be numbers that the pyramid
+//rows end at. For example [1, 3, 6, 10, 15, 21, 28, 36]
+//:param D_r0:
+//
+std::vector<double> generate_zernike_coeff(uint32_t N, double v, cv::RNG &rng)
 {
-    /*
-    Just a simple function to generate random coefficients as needed, conforms to Zernike's Theory. The nollCovMat()
-    function is at the heart of this function.
+    std::vector<double> result;
+    double c1 = std::exp((3.0 / 10.0) * std::log(v));
 
-    A note about the function call of nollCovMat in this function. The input (..., 1, 1) is done for the sake of
-    flexibility. One can call the function in the typical way as is stated in its description. However, for
-    generality, the D/r0 weighting is pushed to the "b" random vector, as the covariance matrix is merely scaled by
-    such value.
+    // C = nollCovMat(N, 1, 1)
+    cv::Mat ncm = noll_covariance_matrix(N, 1, 1);
 
-    :param num_zern: This is the number of Zernike basis functions/coefficients used. Should be numbers that the pyramid
-    rows end at. For example [1, 3, 6, 10, 15, 21, 28, 36]
-    :param D_r0:
-    :return:
-    */
-    
-    // C = nollCovMat(num_zern, 1, 1)
     // e_val, e_vec = np.linalg.eig(C)
+    cv::Mat e_val, e_vec;
+    eigen(ncm, e_val, e_vec);
+
     // R = np.real(e_vec * np.sqrt(e_val))
+    cv::sqrt(e_val, e_val);
+    e_val = cv::Mat::diag(e_val);
+    cv::Mat R = e_vec * e_val;
+    //R = get_real(R);
 
-    // b = np.random.randn(int(num_zern), 1) * D_r0 ** (3.0/10.0)
+    // b = np.random.randn(int(num_zern), 1) * v ** (3.0/10.0)
+    cv::Mat b(N, 1, CV_64FC1);
+    rng.fill(b, cv::RNG::NORMAL, 0.0, 1.0);
+    b *= (c1 * c1);
+
     // a = np.matmul(R, b)
+    //cv::Mat dst = R * b;
+    cv::Mat a;
+    a = R * b;
 
-    // return a
-}
+    cv::MatIterator_<double> itr;
+    cv::MatIterator_<double> end;
+
+    for (itr = a.begin<double>(), end = a.end<double>(); itr != end; ++itr)
+    {
+        result.push_back(*itr);
+    }
+
+    return result;
+
+}   // end of generate_zernike_coeff
+
+//-----------------------------------------------------------------------------
+cv::Mat radial_zernike(int64_t n, int64_t m, cv::Mat& x_grid, cv::Mat& y_grid)
+{
+    uint64_t idx;
+    double tmp;
+    cv::Mat xx = x_grid.mul(x_grid);
+    cv::Mat yy = y_grid.mul(y_grid);
+    cv::Mat rho = xx + yy;
+    cv::Mat tmp_pow;
+    double t1, t2, t3, t4;
+
+    //rho = np.sqrt(x_grid * *2 + y_grid * *2)
+    cv::sqrt(rho, rho);
+
+    //radial = np.zeros(rho.shape)
+    cv::Mat radial = cv::Mat::zeros(rho.size(), CV_64FC1);
+
+    m = std::abs(m);
+
+    //for k in range(int((n - m) / 2 + 1)) :
+    for (idx = 0; idx < (uint64_t)(((n - m) / 2.0) + 1); ++idx)
+    {
+        t1 = ((idx & 0x01 == 1) ? -1.0 : 1.0);
+        t2 = std::tgamma(n - idx + 1);
+        t3 = (std::tgamma(idx + 1) * tgamma(((n + m) / 2.0) - idx + 1) * tgamma(((n - m) / 2.0) - idx + 1));
+        // temp = (-1) * *k * np.math.factorial(n - k) / (np.math.factorial(k) * np.math.factorial((n + m) / 2 - k) * np.math.factorial((n - m) / 2 - k))
+        tmp = ((idx & 0x01 == 1) ? -1.0 : 1.0) * std::tgamma(n - idx + 1) / (std::tgamma(idx + 1) * tgamma(((n + m) / 2.0) - idx + 1) * tgamma(((n - m) / 2.0) - idx + 1));
+
+        //    radial += temp * rho * *(n - 2 * k)
+        cv::pow(rho, (n - 2 * idx), tmp_pow);
+        radial += (tmp * tmp_pow);
+    }
+
+    return radial;
+
+}   // end of radial_zernike
 
 
 
+//-----------------------------------------------------------------------------
+// This function simply
+// : param index :
+// : param x_grid :
+// : param y_grid :
+//
+cv::Mat generate_zernike_poly(int64_t N, cv::Mat& x_grid, cv::Mat& y_grid)
+{
+    int64_t n, m;
+    cv::Mat tmp = cv::Mat::zeros(x_grid.size(), CV_64FC1);
+
+    noll2zernike_index(N, n, m);
+
+    //radial = radialZernike(x_grid, y_grid, (n, m))
+    cv::Mat radial = radial_zernike(n, m, x_grid, y_grid);
+    cv::Mat dst;
+
+    cv::MatIterator_<double> x_itr = x_grid.begin<double>();
+    cv::MatIterator_<double> x_end = x_grid.end<double>();
+    cv::MatIterator_<double> y_itr = y_grid.begin<double>();
+    cv::MatIterator_<double> tmp_itr = tmp.begin<double>();
+
+    if (m < 0)
+    {
+        // return np.multiply(radial, np.sin(-m * np.arctan2(y_grid, x_grid)))
+        for (; x_itr != x_end; ++x_itr, ++y_itr, ++tmp_itr)
+        {
+            *tmp_itr = std::sin((-m) * std::atan2(*y_itr, *x_itr));
+        }
+    }
+    else
+    {
+        // return np.multiply(radial, np.cos(m * np.arctan2(y_grid, x_grid)))
+        for (; x_itr != x_end; ++x_itr, ++y_itr, ++tmp_itr)
+        {
+            *tmp_itr = std::cos(m * std::atan2(*y_itr, *x_itr));
+        }
+    }
+
+    dst = radial.mul(tmp);
+    return dst;
+}   // generate_zernike_poly
+
+
+//-----------------------------------------------------------------------------
+// Generating the Zernike Phase representation.
+// This implementation uses Noll's indices. 1 -> (0,0), 2 -> (1,1), 3 -> (1, -1), 4 -> (2,0), 5 -> (2, -2), etc.
+//def zernikeGen(N, coeff, **kwargs)
+// 
+//void generate_zernike_phase(int64_t N, std::vector<double>& coeff, std::vector<cv::Mat>& dst)
+void generate_zernike_phase(int64_t N, std::vector<double>& coeff, cv::Mat& zern_out)
+{
+    uint64_t idx;
+    cv::Mat x_grid, y_grid;
+
+    //num_coeff = coeff.size
+    uint64_t num_coeff = coeff.size();
+
+    // Setting up 2D grid
+    //x_grid, y_grid = np.meshgrid(np.linspace(-1, 1, N, endpoint = True), np.linspace(-1, 1, N, endpoint = True))
+    meshgrid(-1.0, 1.0, N, -1.0, 1.0, N, x_grid, y_grid);
+
+    //#mask = np.sqrt(x_grid * *2 + y_grid * *2) <= 1
+    //#x_grid = x_grid * mask
+    //#y_grid = y_grid * mask
+
+    //zern_out = np.zeros((N, N, num_coeff))
+    zern_out = cv::Mat::zeros(N, N, CV_64FC1);
+    //dst.clear();
+
+    for (idx = 0; idx < num_coeff; ++idx)
+    {
+        // zern_out[:, : , i] = coeff[i] * genZernPoly(i + 1, x_grid, y_grid)
+        zern_out += generate_zernike_poly(idx + 1, x_grid, y_grid);
+        //dst.push_back(coeff[idx] * zern_out);
+    }
+
+}   // end of generate_zernike_phase
+
+
+//-----------------------------------------------------------------------------
+//def psfGen(N, **kwargs) :
+//    EXAMPLE USAGE - TESTING IN MAIN
+//    temp, xx, yy, pupil = util.psfGen(256, pad_size = 1024)
+//    psf = np.abs(temp) * *2
+//    print(np.min(xx.ravel()), np.max(xx.ravel()))
+//    plt.imshow(psf / np.max(psf.ravel()), extent = [np.min(xx.ravel()), np.max(xx.ravel()),
+//        np.min(yy.ravel()), np.max(yy.ravel())])
+//    plt.show()
+//
+//    :param N :
+//: param kwargs :
+// generate_psf(NN, coeff=aa, L=p_obj['L'], D=p_obj['D'], z_i=1.2, wavelength=p_obj['wvl'])
+void generate_psf(uint64_t N, param_obj &p, std::vector<double> &coeff, double z_i, cv::Mat& dst)
+{
+    
+    cv::Mat x_grid, y_grid;
+    cv::Mat mask;
+
+    //    wavelength = kwargs.get('wavelength', 500 * (10 * *(-9)))
+    //    pad_size = kwargs.get('pad_size', 0)
+    //    D = kwargs.get('D', 0.1)
+    //    L = kwargs.get('L', -1)
+    //    z_i = kwargs.get('z_i', 1.2)
+    double pad_saize = 0.0;
+    
+    //    vec = kwargs.get('coeff', np.asarray([[1], [0], [0], [0], [0], [0], [0], [0], [0]] ))
+    //    x_grid, y_grid = np.meshgrid(np.linspace(-1, 1, N, endpoint = True), np.linspace(-1, 1, N, endpoint = True))
+    meshgrid(-1.0, 1.0, N, -1.0, 1.0, N, x_grid, y_grid);
+
+    //    mask = np.sqrt(x_grid * *2 + y_grid * *2) <= 1
+    x_grid = x_grid.mul(x_grid);
+    y_grid = y_grid.mul(y_grid);
+    mask = x_grid + y_grid;
+    sqrt_cmplx(mask, mask);
+    mask = (mask <= 1);
+    
+    //    zernike_stack = zernikeGen(N, vec)
+    //    phase = np.sum(zernike_stack, axis = 2)
+    std::vector<cv::Mat> zernike_stack;
+    cv::Mat phase;
+    generate_zernike_phase(N, coeff, phase);
+        
+    //    wave = np.exp((1j * 2 * np.pi * phase)) * mask
+    cv::Mat wave;
+
+
+    //    pad_wave = np.pad(wave, int(pad_size / 2), 'constant', constant_values = 0)
+    
+    
+    //    #c_psf = np.fft.fftshift(np.fft.fft2(pad_wave))
+    //    h = np.fft.fftshift(np.fft.ifft2(pad_wave))
+    
+    
+    //    #pad_wave = np.abs(pad_wave) * *2
+    //    # numpy.correlate(x, x, mode = 'same')
+    //
+    //    #plt.imshow(phase * mask)
+    //    #plt.show()
+    //
+    //    M = pad_size + N
+    
+    
+    //    fs = N * wavelength * z_i / D
+    
+    
+    //    temp = np.linspace(-fs / 2, fs / 2, M)
+    
+    
+    //    x_samp_grid, y_samp_grid = np.meshgrid(temp, -temp)
+    
+    //    if L == -1:
+    //return h, x_samp_grid, y_samp_grid, phase* mask,
+    //    else:
+    //return h, (L / z_i)* x_samp_grid, (L / z_i)* y_samp_grid, phase* mask, wave
+
+}   // end of generate_psf
 
 
 #endif  // _ZERNIKE_FUNCTIONS_H_
