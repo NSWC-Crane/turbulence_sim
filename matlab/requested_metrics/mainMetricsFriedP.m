2296f6cd385a5777b64ea721f9daf22b17a69be0
Lockridge <1516627391117002@mil>
2022-10-05 15:55:54 -0400

    Change to add Atmos and test new metrics

--- matlab/requested_metrics/mainMetricsFriedP.m
+++ matlab/requested_metrics/mainMetricsFriedP.m
@@ -1,48 +1,48 @@
 % Get average metrics for all zoom and range values
-% calculate r0 Fried parameter for all zoom and range values
 % Plot average metric to r0
 % For each metric, for each zoom and range value, call FilterMetrics
-% function to return metric for 20 values.  Create a matrix with zoom, 
-% range, r0, and average metric value for each metric
+%    function to return metric for 20 values.  Create a matrix with zoom, 
+%    range, r0, and average metric value for each metric
 % Plot average metric value as a function of r0
 
-% Add r0 to Excel spreadsheet 
 % For each zoom and range, pull cn2 and r0 into new Matrix
-% Then add columns for each metric in new matrix
 
 % Metrics:  'FFT of Diff Laplacian', 'Diff of FFT Laplacian','FFT of Diff LoGs',
-% 'Diff of FFT LoGs','Absolute Difference'
-% C:\Data\JSSAP\combined_sharpest_images_withCn2.xlsx
+% 'Diff of FFT LoGs','Absolute Difference', 'Absolute FFT'
+%
 
 clear
 clc
 
-rangeV = transpose(600:50:1000);
-zoomV = [2000;2500;3000;3500;4000;5000];
-col1 = repelem(rangeV, length(zoomV));
-col2 = repmat(zoomV, length(rangeV),1);
-MetricsF = [col1 col2];
+% Set up table with columns zoom, range, cn2, r0 using data file:
+% C:\Data\JSSAP\combined_sharpest_images_withCn2FriedPar.xlsx
+T1 = readtable('C:\Data\JSSAP\combined_sharpest_images_withAtmos.xlsx','Sheet','combined_sharpest_images', ...
+    'Range', 'D1:E55');
+T2 = readtable('C:\Data\JSSAP\combined_sharpest_images_withAtmos.xlsx','Sheet','combined_sharpest_images', ...
+    'Range','T1:U55');
+T = [T1 T2];
 
-% For columns 3 and 4, read in cn2 and r0 columns in file named  
-% C:\Data\JSSAP\combined_sharpest_images_withCn2.xlsx
-col34 = xlsread('C:\Data\JSSAP\combined_sharpest_images_withCn2FriedPar.xlsx','combined_sharpest_images_withCn','N:O');
-MetricsF = [MetricsF col34];
+% To find range/value in table:
+% ind = (T.zoom == 2000) & (T.range == 600);
+% T{ind,'cn2'}
+% T{ind,'r0'}
+
 
 % Call function FilterMetrics for each filter and then each range, zoom
 % combination
 % Setup matrices for each metric
 AvgFDL = zeros(54,1);
 AvgDFL = zeros(54,1);
-AvgFDLoG = zeros(54,2);
-AvgDFLoG = zeros(54,2);
+AvgFDLoG = zeros(54,4);
+AvgDFLoG = zeros(54,4);
 AvgAbDiff = zeros(54,1);
 AvgAbDiffFFT = zeros(54,1);
 
 % Note MetricsF(:,1) is range
 % Note MetricsF(:,2) is zoom
-for indx = 1: length(MetricsF)
+for indx = 1: height(T)
     
-         [dirBase, dirSharp, basefileN, ImgNames] = GetImageInfo(MetricsF(indx,1), MetricsF(indx,2));
+         [dirBase, dirSharp, basefileN, ImgNames] = GetImageInfo(T{indx,1}, T{indx,2});
 %         sigma = 1:1;
 %         [metrics1] = FilterMetrics('FFT of Diff Laplacian', dirBase, dirSharp, basefileN, ImgNames, sigma);
 %         AvgFDL(indx) = mean(metrics1);
@@ -56,10 +56,10 @@ for indx = 1: length(MetricsF)
 %         avgs = mean(metrics3);
 %         AvgFDLoG(indx,1:4) = avgs;
 
-        sigma = 1:2;
-        [metrics4] = FilterMetrics('Diff of FFT LoGs', dirBase, dirSharp, basefileN, ImgNames, sigma);
-        avgs = mean(metrics4);
-        AvgDFLoG(indx,1:2) = avgs;
+%         sigma = 0.2:0.2:0.6;
+%         [metrics4] = FilterMetrics('Diff of FFT LoGs', dirBase, dirSharp, basefileN, ImgNames, sigma);
+%         avgs = mean(metrics4);
+%         AvgDFLoG(indx,1:3) = avgs;
 
 %         sigma = 1:1;
 %         [metrics5] = FilterMetrics('Absolute Difference', dirBase, dirSharp, basefileN, ImgNames, sigma);
@@ -77,7 +77,7 @@ symbs = ['*','*','*','*','*','*','*','s','s'];
 sz = 8;
 
 % figure()
-% gscatter(MetricsF(:,4), AvgAbDiff, MetricsF(:,1),[],symbs, sz) %, 'filled') %'LineWidth',2)
+% gscatter(T{:,4}, AvgAbDiff, MetricsF(:,1),[],symbs, sz) %, 'filled') %'LineWidth',2)
 % xlabel('Fried Parameter r_0')
 % ylabel('Metric')
 % title('Absolute Difference of Images')
@@ -86,25 +86,26 @@ sz = 8;
 % exportgraphics(f,outf,'Resolution',300)
 % 
 figure()
-gscatter(MetricsF(:,4), AvgAbDiffFFT, MetricsF(:,1),[],symbs, sz) %, 'filled') %'LineWidth',2)
+gscatter(log10(T{:,4}), log10(AvgAbDiffFFT), T{:,1},[],symbs, sz) %, 'filled') %'LineWidth',2)
 xlabel('Fried Parameter r_0')
 ylabel('Metric')
 title('Absolute Difference of FFT of Images')
-outf = "C:\Projects\JSSAP\MetricsPlots\avgFried\absdiff.png";
-f = gcf;
-exportgraphics(f,outf,'Resolution',300)
+%outf = "C:\Projects\JSSAP\MetricsPlots\avgFried\absdiff.png";
+%f = gcf;
+%exportgraphics(f,outf,'Resolution',300)
 
 figure()
-gscatter(MetricsF(:,4), AvgDFL, MetricsF(:,1),[],symbs,sz) %, 'filled') %'LineWidth',2)
+gscatter((T{:,4}), log10(AvgDFL), T{:,1},[],symbs,sz) %, 'filled') %'LineWidth',2)
 xlabel('Fried Parameter r_0')
 ylabel('Metric')
 title('Difference of FFT of Laplacian Images')
-outf = "C:\Projects\JSSAP\MetricsPlots\avgFried\avgDFLap_Modified.png";
-f = gcf;
-exportgraphics(f,outf,'Resolution',300)
+%outf = "C:\Projects\JSSAP\MetricsPlots\avgFried\avgDFLap_Modified.png";
+%f = gcf;
+%exportgraphics(f,outf,'Resolution',300)
+
 % 
 % figure()
-% gscatter(MetricsF(:,4), AvgFDL, MetricsF(:,1),[],symbs,sz) %, 'filled') %'LineWidth',2)
+% gscatter(T{:,4}, AvgFDL, T{:,1},[],symbs,sz) %, 'filled') %'LineWidth',2)
 % xlabel('Fried Parameter r_0')
 % ylabel('Metric')
 % title('FFT of Difference of Laplacian Images')
@@ -114,7 +115,7 @@ exportgraphics(f,outf,'Resolution',300)
 
 % for k = 1:2
 %     figure()
-%     gscatter(MetricsF(:,4), AvgFDLoG(:,k), MetricsF(:,1),[],symbs,sz) %, 'filled') %'LineWidth',2)
+%     gscatter(T{:,4}, AvgFDLoG(:,k), T{:,1},[],symbs,sz) %, 'filled') %'LineWidth',2)
 %     xlabel('Fried Parameter r_0')
 %     ylabel('Metric')
 %     title("FFT of Difference of LoG Images, Sigma " + num2str(k))
@@ -123,23 +124,25 @@ exportgraphics(f,outf,'Resolution',300)
 %     exportgraphics(f,outf,'Resolution',300)
 % end
 
-for k = 1:2
-    figure()
-    gscatter(MetricsF(:,4), AvgDFLoG(:,k), MetricsF(:,1),[],symbs,sz) %, 'filled') %'LineWidth',2)
-    xlabel('Fried Parameter r_0')
-    ylabel('Metric')
-    title("Difference of FFT of LoG Images, Sigma " + num2str(k))
-    outf = "C:\Projects\JSSAP\MetricsPlots\avgFried\avgDFLoG_S_Modified" + num2str(k) + ".png";
-    f = gcf;
-    exportgraphics(f,outf,'Resolution',300)
-end
-
-figure()
-gscatter(MetricsF(:,3), MetricsF(:,4), MetricsF(:,1),[],symbs,sz) %, 'filled') %'LineWidth',2)
-xlabel('Cn^2')
-ylabel('Fried Parameter')
-title('Fried Parameter vs Cn^2')
-outf = "C:\Projects\JSSAP\MetricsPlots\avgFried\friedVsCn2.png";
-f = gcf;
-exportgraphics(f,outf,'Resolution',300)
+% sigma = 0.2:0.2:0.6;
+% for k = 1:3
+%     figure()
+%     gscatter((T{:,3}), log10(AvgDFLoG(:,k)), T{:,1},[],symbs,sz) %, 'filled') %'LineWidth',2)
+%     %xlabel('Fried Parameter r_0')
+%     xlabel('Cn_2')
+%     ylabel('Metric')
+%     title("Difference of FFT of LoG Images, Sigma " + num2str(sigma(k)))
+%     outf = "C:\Projects\JSSAP\MetricsPlots\avgFried\avgDFLoG_S_ModifiedSig" + num2str(k) + ".png";
+%     f = gcf;
+%     %exportgraphics(f,outf,'Resolution',300)
+% end
+% 
+% figure()
+% gscatter(T{:,4}, T{:,3}, T{:,1},[],symbs,sz) %, 'filled') %'LineWidth',2)
+% xlabel('Cn^2')
+% ylabel('Fried Parameter')
+% title('Fried Parameter vs Cn^2')
+% outf = "C:\Projects\JSSAP\MetricsPlots\avgFried\friedVsCn2.png";
+% f = gcf;
+% %exportgraphics(f,outf,'Resolution',300)
 
