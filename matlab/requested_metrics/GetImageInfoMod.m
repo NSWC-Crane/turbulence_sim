fa389fc199e9f7188e9d712ffca5cc6871cb5ef9
davemers0160 <davemers@iu.edu>
2022-10-18 10:45:40 -0400

    Update GetImageInfoMod.m

--- matlab/requested_metrics/GetImageInfoMod.m
+++ matlab/requested_metrics/GetImageInfoMod.m
@@ -1,7 +1,7 @@
-function [dirModBase, dirSharp, basefileN, ImgNames] = GetImageInfoMod(rangeV, zoomV)
+function [dirModBase, dirSharp, basefileN, ImgNames] = GetImageInfoMod(data_root, rangeV, zoomV)
 
-data_root = "C:\Data\JSSAP\";
-% data_root = "C:\Projects\data\turbulence\";
+% data_root = "C:\Data\JSSAP\";
+%data_root = "C:\Projects\data\turbulence\";
 
 dirBase = data_root + "sharpest\z" + num2str(zoomV);
 dirModBase = data_root + "modifiedBaselines";
