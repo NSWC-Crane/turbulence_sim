d4f3698e61e9c3f630a61e5b86e392a221e8a087
Lockridge <1516627391117002@mil>
2022-10-18 10:52:31 -0400

    Update with fixes for 150 pixels

--- matlab/requested_metrics/mainCorrPatchesAllRngZoom.m
+++ matlab/requested_metrics/mainCorrPatchesAllRngZoom.m
@@ -5,19 +5,20 @@ clearvars
 clc
 
 rangeV = 600:100:1000;
-%rangeV = [1000];
+%rangeV = [700];
 zoom = [2000, 2500, 3000, 3500, 4000, 5000];
-%zoom = [5000];
+%zoom = [3000];
 
 szPatch = 64;
 lKernel = 0.25*[0,-1,0;-1,4,-1;0,-1,0];
-%lKernel = [1, -2, 1];
 
 dirOut = "C:\Data\JSSAP\modifiedBaselines\CorrPlots_Patches";
 ccZ = [];
+ccZl = [];
 for rng = rangeV
     figure()
     x= 0:19;
+    zmleg = [];
     for zm = zoom
         [dirBase, dirSharp, basefileN, ImgNames] = GetImageInfoMod(rng, zm);
 
@@ -27,21 +28,28 @@ for rng = rangeV
         %ImgB = imgaussfilt(ImgB,0.5, 'FilterSize',9, 'Padding', 'symmetric');
         ImgR = double(imread(fullfile(dirSharp, ImgNames{1})));
         ImgR = ImgR(:,:,2);  % only green channel
-        
-        [m,n] = size(ImgB);
 
-        % Setup patches
-        numPatches = uint8(m/szPatch) - 1;
+        [img_h, img_w] = size(ImgB);
+        % Setup patches - Assume square images so we'll just use the image height (img_h)
+        numPatches = floor(img_h/szPatch);
+        remaining_pixels = img_h - (szPatch * numPatches);
         
-        intv = uint8((m-(numPatches * szPatch))/(numPatches + 1));
-        % row,col start at intv,intv
+        if (remaining_pixels == 0)
+            remaining_pixels = szPatch;
+            numPatches = numPatches - 1;
+        end
+        
+        intv = floor(remaining_pixels/(numPatches + 1));
+        
+        % Collect ratio without Laplacian
         cc = [];
+        % Collect ratio with Laplacian
         cc_l = [];
-        
+        % Identifier for image (1:i00, 2:i01, etc)
         index = 1;
         
-        for prow = intv:szPatch+intv:numPatches * (szPatch+intv)
-            for pcol = intv:szPatch+intv:numPatches * (szPatch+intv)
+        for prow = intv:szPatch+intv:img_h-szPatch
+            for pcol = intv:szPatch+intv:img_w-szPatch
                        
                 ImgB_patch = ImgB(prow:prow+szPatch-1,pcol:pcol+szPatch-1);
                 ImgR_patch = ImgR(prow:prow+szPatch-1,pcol:pcol+szPatch-1);
@@ -63,7 +71,7 @@ for rng = rangeV
                 lr_fft = fftshift(fft(lapImgR)/numel(lapImgR));
                 diff_fft_lrb = lr_fft - lb_fft;
                 
-                % Autocorrelation/convolution?
+                % Autocorrelation/convolution
                 cv_diff_rb = conv2(diff_fft_rb, conj(diff_fft_rb(end:-1:1, end:-1:1)), 'same');
                 s_01 = sum(abs(cv_diff_rb(:)));
                 
@@ -91,7 +99,7 @@ for rng = rangeV
                     diff_fft_otRb = otr_fft - b_fft;
                     diff_fft_lotRb = lotr_fft - lb_fft;
                     
-                    % Cross convolution?
+                    % Cross correlation/convolution
                     cv_diff_otRb = conv2(diff_fft_rb, conj(diff_fft_otRb(end:-1:1, end:-1:1)), 'same');
                     s_02 = sum(abs(cv_diff_otRb(:)));                
                     
@@ -119,34 +127,24 @@ for rng = rangeV
         avg_r = mean(cc,1);
         avg_rl = mean(cc_l,1);
 
-        ccZ = [ccZ; rng zm avg_rl];
+        ccZ = [ccZ; rng zm avg_r];
+        ccZl = [ccZ; rng zm avg_rl];
 
-        plot(x, avg_rl)
+        plot(x, avg_r)
         hold on
+
+        zmleg = [zmleg; num2str(zm)];
     end
     
     grid on
     xlim([0,19])
-    legend("2000","2500","3000","3500","4000","5000")
-    title("Range " + num2str(rng) + " Patch Size " + num2str(szPatch))
-%     fileN = fullfile(dirOut,"r" + num2str(rng)  + ".png");
+    legend(zmleg)
+    title("NL Range " + num2str(rng) + " Patch Size " + num2str(szPatch))
+%     fileN = fullfile(dirOut,"NL_r" + num2str(rng)  + ".png");
 %     f = gcf;
 %     exportgraphics(f,fileN,'Resolution',300)
     hold off;
 end
-
-% figure
-% for i = 1:
-% 
-%     x= 0:19;
-%     plot(x, avg_rl) %
-%     grid on
-%     xlim([0,19])
-%     title("Range " + num2str(rng) + " Zoom " + num2str(zm) + " Patch Size " + num2str(szPatch))
-%     %fileN = fullfile(dirOut,"r" + num2str(rng) + "_z" + num2str(zm) + ".png");
-%     %f = gcf;
-%     %exportgraphics(f,fileN,'Resolution',300)
-%   hold off
 
 % figure
 % hold on
