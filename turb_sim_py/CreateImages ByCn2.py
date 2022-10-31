f36c8c44206fa1f6d24c25f996f721b6b8a001ce
Lockridge <1516627391117002@mil>
2022-10-31 16:38:20 -0400

    Adds comments, increases number of Cn2 values

--- turb_sim_py/CreateImages ByCn2.py
+++ turb_sim_py/CreateImages ByCn2.py
@@ -1,13 +1,13 @@
 # -*- coding: utf-8 -*-
 """
-Using Purdue code that simulates images with tilt and blur.
+Using Purdue code that simulates images with tilt and blur with D Emerson's improvements.
 Created on Mon Oct 24 09:59:25 2022
-
-@author: victoria.lockridge
 
 Creates simulated images at specific range/zoom values with varying Cn2/r0 values.
 Images are based on Modified Baseline images (no blur).
 Images are saved for use in MATLAB to run similarity metrics.
+
+V Lockridge
 """
 
 from matplotlib import pyplot as plt
@@ -37,8 +37,11 @@ def ChangImgDtypeUint8(img):
 
 D = 0.095
 wvl = 0.525e-6 
-cn2 = [5e-16,1e-15,3e-15,7e-15,1e-14,3e-14,7e-14,1e-13,5e-13]
-numSims = 20
+#cn2 = [5e-16,1e-15,4e-15,7e-15,1e-14,2e-14,3e-14,4e-14,5e-14,6e-14,7e-14,8e-14,9e-14,
+#       1e-13,4e-13,7e-13,1e-12]
+# Add another set
+cn2 = [2e-13,3e-13,5e-13,6e-13,8e-13,9e-13,2e-12,3e-12,4e-12,5e-12,6e-12,7e-12,8e-12,9e-12,1e-11]
+numSims = 20  # Number of simulated images of same zoom/range/cn2 - will average metrics in MATLAB.
 
 if __name__ == '__main__':
     # Define directories
@@ -55,8 +58,8 @@ if __name__ == '__main__':
     dirOut = os.path.join(data_root, r"modifiedBaselines\SimImgs_VaryingCn2") # Save images here
 
     # Select each modified baseline image in dir C:\Data\JSSAP\modifiedBaselines
-    # Create several simulated images using r0 for appropriatge range and cn2 values
-    # Save images for calculating similarity metrics in MATLAB
+    # Create several simulated images (numSims) using r0 for appropriatge range and cn2 values
+    # Save images to use in MATLAB to calculate similarity metrics
     
     # Import file with obj_size information - required to create simulated images
     dfAtm = pd.read_excel(fileA)
@@ -107,7 +110,7 @@ if __name__ == '__main__':
     numc = len(cn2)
     rcls = [numc * [rangeV[i]] for i in range(len(rangeV))]
     dfwrite['range'] = [j for i in rcls for j in i]
-    dfwrite['cn2'] = 9*cn2
+    dfwrite['cn2'] = len(rangeV)*cn2
     dfwrite['r0'] = CalculateR0(dfwrite.cn2,dfwrite.range, wvl)
     # Save range, cn2, r0 values in .csv file for each range for use in MATLAB 
     #                program that calculates metrics.
