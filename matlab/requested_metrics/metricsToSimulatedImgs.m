f9feb0d63e95775ca42738cfc2f3cbdaa7a0bcaf
Emerson <davemers0160@gmail.com>
2022-11-04 14:36:57 -0400

    Update metricsToSimulatedImgs.m

--- matlab/requested_metrics/metricsToSimulatedImgs.m
+++ matlab/requested_metrics/metricsToSimulatedImgs.m
@@ -6,10 +6,10 @@
 clearvars
 clc
 
-rangeV = 600:50:1000;
-%rangeV = [600];
-zoom = [2000, 2500, 3000, 3500, 4000, 5000];
-%zoom = [ 2000, 3000, 4000];
+% rangeV = 600:50:1000;
+rangeV = [600];
+% zoom = [2000, 2500, 3000, 3500, 4000, 5000];
+zoom = [ 2000];
 
 platform = string(getenv("PLATFORM"));
 if(platform == "Laptop")
@@ -27,7 +27,7 @@ dirSims = data_root + "modifiedBaselines\SimImgs_VaryingCn2";
 dirOut = data_root + "modifiedBaselines\SimImgs_VaryingCn2\Plots2";
 
 % Size of subsections of image for metrics
-szPatch = 64;
+szPatch = 148;
 % Laplacian kernel
 lKernel = 0.25*[0,-1,0;-1,4,-1;0,-1,0];
 
@@ -111,7 +111,7 @@ for rng = rangeV
                         % Take Laplacian of baseline patch
                     lapImgB = conv2(ImgB_patch, lKernel, 'same');
                         %  FFT of baseline and then Laplacian
-                    b_fft = fftshift(fft2(ImgB_patch)/numel(ImgB_patch));
+%                     b_fft = fftshift(fft2(ImgB_patch)/numel(ImgB_patch));
                     lb_fft = fftshift(fft2(lapImgB)/numel(lapImgB));
                     
                     % Real Image ℱ(𝛻^2 (〖𝐼𝑚𝑔〗_0−𝜇_0 ))
@@ -121,20 +121,21 @@ for rng = rangeV
                         % Take Laplacian of real image patch
                     lapImgR = conv2(ImgR_patch, lKernel, 'same');
                         %  FFT of baseline and then Laplacian
-                    r_fft = fftshift(fft2(ImgR_patch)/numel(ImgR_patch));
+%                     r_fft = fftshift(fft2(ImgR_patch)/numel(ImgR_patch));
                     lr_fft = fftshift(fft2(lapImgR)/numel(lapImgR));
                     
                     % Difference between FFT of real image and baseline image
-                    diff_fft_rb = r_fft - b_fft;
+%                     diff_fft_rb = r_fft - b_fft;
     
                     % Difference between FFT of Laplacian of real image and 
                     %                                   FFT of baseline image
                     %  EQN 1 Final:  〖𝐼𝑚𝑔〗_0𝑏=ℱ(𝛻^2 (〖𝐼𝑚𝑔〗_0−𝜇_0 ))−ℱ(𝛻^2 (〖𝐼𝑚𝑔〗_𝑏−𝜇_𝑏 ))?
-                    diff_fft_lrb = lr_fft - lb_fft;
+%                     diff_fft_lrb = lr_fft - lb_fft;
+                    diff_fft_lrb = lr_fft;
                     
                     % Autocorrelation/convolution:  Real and baseline image
-                    cv_diff_rb = conv2(diff_fft_rb, conj(diff_fft_rb(end:-1:1, end:-1:1)), 'same');
-                    s_01 = sum(abs(cv_diff_rb(:)));
+%                     cv_diff_rb = conv2(diff_fft_rb, conj(diff_fft_rb(end:-1:1, end:-1:1)), 'same');
+%                     s_01 = sum(abs(cv_diff_rb(:)));
                     % Autocorrelation/convolution:  Laplacian of Real and baseline image
                     % EQN 3: 𝒦_00=𝐸[〖𝐼𝑚𝑔〗_0𝑏∙(〖𝐼𝑚𝑔〗_0𝑏 ) ̅ ]
                     cv_diff_lrb = conv2(diff_fft_lrb, conj(diff_fft_lrb(end:-1:1, end:-1:1)), 'same');
@@ -147,42 +148,105 @@ for rng = rangeV
                     % Laplacian of simulate image i patch
                     lapImgSim = conv2(ImgSim_patch, lKernel, 'same');
                     % FFT of sim image and lap of sim image
-                    sim_fft = fftshift(fft2(ImgSim_patch)/numel(ImgSim_patch));
+%                     sim_fft = fftshift(fft2(ImgSim_patch)/numel(ImgSim_patch));
                     lsim_fft = fftshift(fft2(lapImgSim)/numel(lapImgSim));
                     % Difference between FFTs of sim and base
-                    diff_fft_simRb = sim_fft - b_fft;
+%                     diff_fft_simRb = sim_fft - b_fft;
                     % EQN 2:  Final 〖𝐼𝑚𝑔〗_1𝑏=ℱ(𝛻^2 (〖𝐼𝑚𝑔〗_1−𝜇_1 ))−ℱ(𝛻^2 (〖𝐼𝑚𝑔〗_𝑏−𝜇_𝑏 ))
                     % Difference between FFTs of Laplacians of sim and base
-                    diff_fft_lsimRb = lsim_fft - lb_fft;
+%                     diff_fft_lsimRb = lsim_fft - lb_fft;
+                    diff_fft_lsimRb = lsim_fft;
                     
                     % Cross correlation - Non-Laplacian case
                     % EQN 4:  𝒦_01=𝐸[〖𝐼𝑚𝑔〗_0𝑏∙(〖𝐼𝑚𝑔〗_1𝑏 ) ̅ ]
-                    cv_diff_simRb = conv2(diff_fft_rb, conj(diff_fft_simRb(end:-1:1, end:-1:1)), 'same');
-                    s_02 = sum(abs(cv_diff_simRb(:)));                
+%                     cv_diff_simRb = conv2(diff_fft_rb, conj(diff_fft_simRb(end:-1:1, end:-1:1)), 'same');
+%                     s_02 = sum(abs(cv_diff_simRb(:)));                
                     % Cross correlation - Laplacian case
                     cv_diff_lsimRb = conv2(diff_fft_lrb, conj(diff_fft_lsimRb(end:-1:1, end:-1:1)), 'same');
                     sl_02 = sum(abs(cv_diff_lsimRb(:)));
                     % Ratios Non-Laplacian
-                    r_012 = s_02/s_01;
-                    r = 1-abs(1-r_012);  % Non-Laplacian metric
-                    cc(index) = r;
+%                     r_012 = s_02/s_01;
+%                     r = 1-abs(1-r_012);  % Non-Laplacian metric
+
+
+
+%                     cc(index) = r;
+
                     % Ratios Laplacian
                     % EQN 5 ℳ_01=1−|1−(∑2_0^(𝑗−1)▒𝒦_01 )/(∑2_0^(𝑗−1)▒𝒦_00 )| 
                     %      where 𝑗 is the number of pixels in the image
                     rl012 = sl_02/sl_01;
                     rl = 1-abs(1-rl012);  % Laplacian metric
                     cc_l(index) = rl;
+                   
+                    if(false)
+                        
+                        figure(99);
+                        subplot(1,3,1)
+                        surf(ImgB_patch);
+                        shading interp;
+                        subplot(1,3,2)
+                        surf(ImgR_patch);
+                        shading interp;
+                        subplot(1,3,3)
+                        surf(ImgSim_patch);
+                        shading interp;
+                        colormap(jet(256));                        
+                        
+                        
+                        figure(100);
+                        subplot(1,3,1)
+                        surf(lapImgB);
+                        shading interp;
+                        subplot(1,3,2)
+                        surf(lapImgR);
+                        shading interp;
+                        subplot(1,3,3)
+                        surf(lapImgSim);
+                        shading interp;
+                        colormap(jet(256));
 
+                        figure(101);
+                        subplot(1,3,1)
+                        surf(abs(lb_fft));
+                        shading interp;
+                        subplot(1,3,2)
+                        surf(abs(lr_fft));
+                        shading interp;
+                        subplot(1,3,3)
+                        surf(abs(lsim_fft));
+                        shading interp;
+                        colormap(jet(256));
+
+                        figure(102);
+                        subplot(1,2,1)
+                        surf(abs(diff_fft_lrb));
+                        shading interp;
+                        subplot(1,2,2)
+                        surf(abs(diff_fft_lsimRb));
+                        shading interp;
+                        colormap(jet(256));
+
+                        figure(103);
+                        subplot(1,2,1)
+                        surf(abs(cv_diff_lrb));
+                        shading interp;
+                        subplot(1,2,2)
+                        surf(abs(cv_diff_lsimRb));
+                        shading interp;
+                        colormap(jet(256));                    
+                    end
+                    
                     index = index + 1;
         
                end
             end
             % Calculate mean metric of all patches for this image and save to
             % tables Tm and TmL
-            avg_r = mean(cc);  % non-Laplacian
+%             avg_r = mean(cc);  % non-Laplacian
             avg_rl = mean(cc_l); % Laplacian
     
-            Tm(indT,:) = {rng zm string(cstr{1}) namelist{i} numPatches*numPatches avg_r}; % non-Laplacian
+%             Tm(indT,:) = {rng zm string(cstr{1}) namelist{i} numPatches*numPatches avg_r}; % non-Laplacian
             TmL(indT,:) = {rng zm string(cstr{1}) namelist{i} numPatches*numPatches avg_rl}; % Laplacian
             indT = indT + 1;
         end
@@ -199,7 +263,7 @@ TmL.filename = string(TmL.filename);
 uniqT = unique(TmL(:,[1,2,3]), 'rows', 'stable');
 
 % Use "trubNums.csv" (created by Python file) to find r0 for plotting
-Tr0 = readtable(data_root + "modifiedBaselines\SimImgs_VaryingCn2\turbNums.csv");
+Tr0 = readtable(data_root + "modifiedBaselines\SimImgs_VaryingCn2\turbNums_600.csv");
 Tr0.strcn2 = string(Tr0.cn2);
 Tr0.strcn2 = strrep(Tr0.strcn2,'-','');
 
@@ -234,7 +298,7 @@ T_atmos = readtable(fileA);
 % Sort uniqT 
 uniqT = sortrows(uniqT,["range","zoom","r0"]);
 
-% Create all plots
+%% Create all plots
 for rngP = rangeV
     figure()
     legendL = [];
@@ -284,7 +348,7 @@ for rngP = rangeV
 
 end
 
-% Plot against log(r0) Semi Log Plot
+%% Plot against log(r0) Semi Log Plot
 for rngP = rangeV
     figure()
     legendL = [];
