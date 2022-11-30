2f5bd4da4e35740bf75c967cb45c2b37ccbe0140
Lockridge <1516627391117002@mil>
2022-11-30 12:09:09 -0500

    Create turb_sim_lib_simReals.m

--- matlab/turb_sim_lib_simReals.m
+++ matlab/turb_sim_lib_simReals.m
@@ -1,10 +1,7 @@
-% Modify tub_sim_lib_example to created simulated images of each real
+% Modify tub_sim_lib_example to create simulated images of each real
 % image, using the range/Cn2 of real image.
 % Create plots of mod baseline, real image, simulated image
 % Create 20 simulated images of each real image to run metrics
-% 
-% 
-% 
 
 format long g
 format compact
@@ -53,55 +50,96 @@ end
 
 % Location of modified baseline images
 dirBL = data_root + "modifiedBaselines\";
-% combined_sharpest_images_withAtmos.xlsx in data_root dir
+% combined_sharpest_images_withAtmos.xlsx in data_root directory
 
 % Common parameters
 D = 0.095;
 wavelength = 525e-9;
 
-% Steps
+% Get Cn2 for real image in fileA 
+fileA = data_root + "combined_sharpest_images_withAtmos.xlsx";
+T_atmos = readtable(fileA);
+varnamesA = {'Date', 'Time', 'Time_secs', 'range', 'zoom', 'focus', 'img_filename', ...
+    'img_height', 'img_width', 'pixel_step', 'start', 'stop', 'obj_size', 'Temperature', ...
+    'Humidity', 'Wind_speed', 'Wind_dir', 'Bar_pressure', 'Solar_load', 'Cn2', 'r0' };
+T_atmos = renamevars(T_atmos, T_atmos.Properties.VariableNames, varnamesA);
+
+% Steps:
 % Get list of all mod BL images
-% Determine zoom/range from filename
+% Determine range/zoom from filename
+% Filter list to images for range/zoom
 % Get parameters from combined_sharpest_images_withAtmos.xlsx (Cn2,
 % obj_size)
+% Create 20 simulated images
+% Plot baseline, real, simulated
+
+%rangeV = [700];
+rangeV = 600:50:1000;
+%zoom = [2500];
+zoom = [2000, 2500, 3000, 3500, 4000, 5000];
+% Get list of all mod BL images
+blFiles = dir(fullfile(dirBL, '*.png'));
+blNames = {blFiles(~[blFiles.isdir]).name};
+
+for rng = rangeV
+    ind = 1;
+    blNamelist = []; % list of all baseline image files at this zoom/range
+    for zm = zoom
+        % Filter by range and zoom to get file names of range/zoom
+        if rng < 1000
+            patt = "_z" + num2str(zm) + "_r0" + num2str(rng);
+        else
+            patt = "_z" + num2str(zm) + "_r" + num2str(rng);
+        end 
+        
+        for i = 1:length(blNames)
+            if contains(blNames{:,i},patt)
+                blNamelist{ind,1} = blNames{:,i};
+                %display(namelist{ind})
+                
+                break;
+            end
+        end
+
+        % Get parameters from combined_sharpest_images_withAtmos.xlsx (Cn2,
+        % obj_size)
+        indA = find(T_atmos.range == rng & T_atmos.zoom == zm);
+        Cn2 = T_atmos.Cn2(indA);
+        obj_size = T_atmos.obj_size(indA);
+        L = rng;
+
+        % Read in baseline image
+        readFile = fullfile(dirBL, blNamelist{ind,1});
+        img = double(imread(readFile));
+
+        [img_h, img_w, img_c] = size(img);
+        
+        if(img_c == 3)
+           img = img(:, :, 2); 
+        end
+        
+        img_blur = zeros(img_h * img_w, 1);
+        % create the correct matlab pointers to pass into the function
+        img_t = libpointer('doublePtr', img);
+        img_blur_t = libpointer('doublePtr', img_blur);
+        calllib(lib_name, 'init_turbulence_params', img_w, D, L, Cn2, wavelength, obj_size);
 
 
-%%%%%%%%%%% HERE %%%%%%%%%%%%%
-%filename = data_root + "sharpest\z5000\baseline_z5000_r1000.png";
 
-img = double(imread(filename));
+        % Create 20 simulated images (make sure to save as uint8)
+        for idx=1:20
+            calllib(lib_name, 'apply_turbulence', img_w, img_h, img_t, img_blur_t);
+        
+            img_blur = reshape(img_blur_t.Value, [img_h, img_w])';
+            img_blur = uint8(img_blur);
+            fileI = "NewSim_r" + num2str(rng) + "_z" + num2str(zm) + "_N" + num2str(idx) + ".png";
+            pathI = data_root + "modifiedBaselines\NewSimulations\SimReal\" + fileI;
+            imwrite(img_blur, pathI);
+        
+        end
 
-[img_h, img_w, img_c] = size(img);
-
-if(img_c == 3)
-   img = img(:, :, 2); 
+    end
+    ind = ind +1;
 end
-
-img_blur = zeros(img_h * img_w, 1);
-
-% create the correct matlab pointers to pass into the function
-img_t = libpointer('doublePtr', img);
-img_blur_t = libpointer('doublePtr', img_blur);
-
-% Varying parameters
-L = 1000;
-Cn2 = 1e-13;
-obj_size = img_w * 0.004276;
-
-calllib(lib_name, 'init_turbulence_params', img_w, D, L, Cn2, wavelength, obj_size);
-
-for idx=1:20
-    calllib(lib_name, 'apply_turbulence', img_w, img_h, img_t, img_blur_t);
-
-    img_blur = reshape(img_blur_t.Value, [img_h, img_w])';
-
-    figure(1);
-    image(uint8(img_blur));
-    colormap(gray(256))
-    pause(1);
-
-end
-
-bp = 1;
 
 unloadlibrary(lib_name);
