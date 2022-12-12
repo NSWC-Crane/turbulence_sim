71a9532a770210880ea5a07811d2ec413d769a68
Lockridge <1516627391117002@mil>
2022-12-12 09:03:59 -0500

    Update plots for clarity

--- matlab/requested_metrics/Test_Cn2Varying/testCn2VaryingResults_NonLap1.m
+++ matlab/requested_metrics/Test_Cn2Varying/testCn2VaryingResults_NonLap1.m
@@ -24,6 +24,33 @@ ref_image1_Files = "C:\Data\JSSAP\sharpest\z3000\0600\image_z02996_f47005_e15004
 test_images1_Files = ["C:\Data\JSSAP\modifiedBaselines\NewSimulations\ByVaryingCn2\NewSim_r600_z3000_c3e14_N1.png",...
     "C:\Data\JSSAP\modifiedBaselines\NewSimulations\ByVaryingCn2\NewSim_r600_z3000_c5e15_N1.png"];
 
+rngstr = "600";
+zmstr = "3000";
+% Read in real image
+refImg = double(imread(ref_image1_Files));
+refImg= refImg(:,:,2);  % Only green channel
+% Subtract Mean of Image
+refImg= refImg - mean(refImg,'all');
+% Remove 5 pixels off borders
+refImg = refImg(5:end-5, 5:end-5);
+
+% Read in max metric test image
+testImgMax = double(imread(test_images1_Files(1)));
+% Subtract Mean of Image
+testImgMax= testImgMax - mean(testImgMax,'all');
+% Remove 5 pixels off borders
+testImgMax = testImgMax(5:end-5, 5:end-5);
+
+% Read in closest to real test image
+testImgClose = double(imread(test_images1_Files(2)));
+% Subtract Mean of Image
+testImgClose= testImgClose - mean(testImgClose,'all');
+% Remove 5 pixels off borders
+testImgClose = testImgClose(5:end-5, 5:end-5);
+
+m = turbulence_metric_noBL_testVL(refImg, testImgMax,"mm", rngstr, zmstr);
+
+m2 = turbulence_metric_noBL_testVL(refImg, testImgClose,"c", rngstr, zmstr);
 
 % Range 600, Zoom 2500
 % Real:  C:\Data\JSSAP\sharpest\z2500\0600\image_z02498_f46581_e15004_i00.png
@@ -33,28 +60,33 @@ ref_image2_Files = "C:\Data\JSSAP\sharpest\z2500\0600\image_z02498_f46581_e15004
 test_images2_Files = ["C:\Data\JSSAP\modifiedBaselines\NewSimulations\ByVaryingCn2\NewSim_r600_z2500_c1e13_N1.png",...
     "C:\Data\JSSAP\modifiedBaselines\NewSimulations\ByVaryingCn2\NewSim_r600_z2500_c1e14_N1.png"];
 
-refImg = double(imread(ref_image1_Files));
+rngstr = "600";
+zmstr = "2500";
+% Read in real image
+refImg = double(imread(ref_image2_Files));
 refImg= refImg(:,:,2);  % Only green channel
 % Subtract Mean of Image
 refImg= refImg - mean(refImg,'all');
 % Remove 5 pixels off borders
 refImg = refImg(5:end-5, 5:end-5);
 
-testImgMax = double(imread(test_images1_Files(1)));
+% Read in max metric test image
+testImgMax = double(imread(test_images2_Files(1)));
 % Subtract Mean of Image
 testImgMax= testImgMax - mean(testImgMax,'all');
 % Remove 5 pixels off borders
 testImgMax = testImgMax(5:end-5, 5:end-5);
 
-testImgClose = double(imread(test_images1_Files(2)));
+% Read in closest to real test image
+testImgClose = double(imread(test_images2_Files(2)));
 % Subtract Mean of Image
 testImgClose= testImgClose - mean(testImgClose,'all');
 % Remove 5 pixels off borders
 testImgClose = testImgClose(5:end-5, 5:end-5);
 
-m = turbulence_metric_noBL_testVL(refImg, testImgMax);
+m3 = turbulence_metric_noBL_testVL(refImg, testImgMax,"mm", rngstr, zmstr);
 
-m2 = turbulence_metric_noBL_testVL(refImg, testImgClose);
+m4 = turbulence_metric_noBL_testVL(refImg, testImgClose,"c", rngstr, zmstr);
 
 
 
